ad5c155e21ffe7dfd02ec359cdec4e0fcd3fbd6c
Илья Аникин <anikin.idm@yandex.ru>
2024-07-28 20:38:15 +0300

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Илья Аникин on 28.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
